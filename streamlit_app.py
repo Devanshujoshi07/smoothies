5a36b29eec3268ead281078c4f3b1444b9c2004f
Devanshujoshi07 <91048658+Devanshujoshi07@users.noreply.github.com>
2024-09-21 12:25:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
-pip install requirement.txt
+import snowflake
+from snowflake import snowpark.context
 # Write directly to the app
 st.title("CUSTOMIZE YOUR SMOOTHIE ")
 st.write(
